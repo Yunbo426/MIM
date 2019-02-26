f89591b92aecf2d5cdbc26c64558b746a49bcf01
张建晋 <swjxzjj@gmail.com>
2019-02-26 10:36:11 +0800

    add data dictionary
    add data dictionary

---
+++ data/human36m.sh
@@ -0,0 +1,25 @@
+# Download H36M images
+mkdir human
+cd human
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S1.tar
+tar -xf S1.tar
+rm S1.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S5.tar
+tar -xf S5.tar
+rm S5.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S6.tar
+tar -xf S6.tar
+rm S6.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S7.tar
+tar -xf S7.tar
+rm S7.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S8.tar
+tar -xf S8.tar
+rm S8.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S9.tar
+tar -xf S9.tar
+rm S9.tar
+wget http://visiondata.cis.upenn.edu/volumetric/h36m/S11.tar
+tar -xf S11.tar
+rm S11.tar
+cd ..
